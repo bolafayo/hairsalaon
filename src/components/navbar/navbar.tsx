56ba27b20d996ee42fd0e2c4cb16a17f1bf412b0
Bolade Olufayo Taiwo <olufayobolade@gmail.com>
2025-08-23 19:12:31 +0100

    added all the page to the navigation bar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -4,14 +4,22 @@ export default function Navbar() {
     
   return (
     
-    <nav className='absolute top-0 left-0 right-0 flex justify-between items-center px-6 py-4 text-white z-10 '>
-      <div className=" text-white font-bebasNeue text-4xl ">Hairs</div>
+    <nav className='absolute top-0 left-0 right-0 flex justify-between items-center px-6 py-4 text-primary z-15 '>
+      
+      <div className=" text-primary font-bebasNeue text-5xl mx-5 ">Hairs</div>
         
-      <ul className='className="font-bebasNeue hidden md:flex space-x-8   text-1xl tracking-wide text-white gap-18 '>
-        <li ><Link to="/homepage" className="hover:opacity-80">HOME</Link></li>
+      <ul className='className="font-bebasNeue hidden md:flex space-x-8   text-1xl tracking-wide text-primary gap-18 '>
+        <li ><Link to="/about" className="hover:opacity-80">ABOUT</Link></li>
         <li><Link to="/services" className="hover:opacity-80">SERVICES</Link></li>
-        <li><Link to="/contact" className="hover:opacity-80">CONTACT</Link></li>
-        {/* <li><Link to="/registration">Registration</Link></li> */}
+        <li><a href="#contact" className="hover:opacity-80">CONTACT</a></li>
+        <li>
+        <Link
+            to="/connect"
+            className="border border-white px-4 py-2 text-sm hover:bg-white hover:text-black transition"
+          >
+            GET IN TOUCH
+          </Link>
+        </li>
       </ul>
     
    
