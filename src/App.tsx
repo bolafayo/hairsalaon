0e81ace5ffa8bdb000706b6a0687aac5e43839a4
Bolade Olufayo Taiwo <olufayobolade@gmail.com>
2025-08-23 19:14:00 +0100

    added a theme for color

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import './App.css'
 import { BrowserRouter , Routes, Route } from "react-router-dom";
 import Homepage from "./components/homepage/homepage";
 import { ThemeProvider } from "./theme/themeprovider"
-
+import About from './components/homepage/about';
+import Services from './components/homepage/services';
+import Connect from './components/homepage/connect';
 
 
 function App() {
@@ -14,8 +16,11 @@ function App() {
     <BrowserRouter>
     
     <Routes>
+   
       <Route path='/' element={<Homepage />}></Route>
-      
+      <Route path="/about" element={<About />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/Connect" element={<Connect />} />
     </Routes>
     </BrowserRouter>
     
